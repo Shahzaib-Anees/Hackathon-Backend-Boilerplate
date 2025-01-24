cf8b648d140981a3d8579967cce0659565cba7be
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2025-01-25 00:03:24 +0500

    Authentication added

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./src/db/index.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./src/routes/user.routes.js";
 dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 app.use("/", async (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use("/api/v1/auth", authRoutes);
+
+(async () => {
+  try {
+    const res = await connectDB();
+    console.log(res);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
