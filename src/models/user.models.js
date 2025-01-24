1f233fd3b1a5525596b6e6dfc79ee02bf1c7148f
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2025-01-24 23:30:49 +0500

    Removing .env

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,2 +1,17 @@
 import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Email is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  
+});
