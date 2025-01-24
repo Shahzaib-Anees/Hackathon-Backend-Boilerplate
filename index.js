0fc10730dd3929a897950057f8ec5364b4efd811
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2025-01-24 22:52:43 +0500

    Adding async function

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
+app.get("/", async(req, res) => {
   res.send("Hello World!");
 });
