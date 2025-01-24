01d681eea24b1150ad1ea46119781591fa7f3750
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2025-01-24 22:57:40 +0500

    Adding port

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get("/", async(req, res) => {
+const PORT = process.env.PORT || 3000;
+app.use("/", async (req, res) => {
   res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
